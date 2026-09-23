c687548023a0e9ea673680ceff91a42a1212f1e8
sandramartinez <alvmart2010@gmail.com>
2026-09-22 20:58:20 -0500

    se agrega actividad de concurrencia

---
+++ code/thread-hilo.py
@@ -0,0 +1,24 @@
+import threading, time
+
+
+def print_numbers():
+    for i in range(1, 6):
+        print(f"Numero: {i}")
+        time.sleep(1)
+
+
+def print_letters():
+    for letter in "ABCD":
+        print(f"letter: {letter}")
+        time.sleep(1)
+
+
+if __name__ == "__main__":
+    h1 = threading.Thread(target=print_numbers)
+    h2 = threading.Thread(target=print_letters)
+
+    h1.start()
+    h2.start()
+
+    h1.join()
+    h2.join()
